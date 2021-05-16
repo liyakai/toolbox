a43e51ca65138ecc540a7f1bed182d33bed5c6b3
liyakai <liyakai@mail.ustc.edu.cn>
2021-05-16 11:07:09 +0800

    网络库工作暂存

--- network/network_mgr.cpp
+++ network/network_mgr.cpp
@@ -1,4 +1,64 @@
-#include "network_mgr.h"
+#include "network/network_mgr.h"
 
 NetworkMaster::NetworkMaster()
+{
+    stop_.store(false);
+}
+
+NetworkMaster::~NetworkMaster()
 {}
+
+bool NetworkMaster::Start()
+{
+    stop_.store(false);
+    worker_.reset(new std::thread([this](){
+        while(!stop_.load())
+        {
+            // 处理主线程发来的事件
+            // 驱动网络更新
+            for(auto& network : networks_)
+            {
+                if(network)
+                {
+                    network->Update();
+                }
+            }
+        }
+    }));
+    return true;
+}
+void NetworkMaster::Update()
+{
+
+}
+
+void NetworkMaster::StopWait()
+{
+    stop_.store(true);
+    if(worker_)
+    {
+        worker_->join();
+        worker_.reset();
+    }
+    while (!event2main_.Empty())
+    {
+        NetEventWorker* event;
+        if(event2main_.Read<NetEventWorker*>(event))
+        {
+            MemPoolMgr->GiveBack((char*)event);
+        }
+    }
+    for(auto& network : networks_)
+    {
+        network.reset();
+    }
+
+}
+void NetworkMaster::Close(NetworkType type, uint64_t conn_id)
+{
+
+}
+void NetworkMaster::Send(NetworkType type, uint64_t conn_id, const char* data, uint32_t size) 
+{
+
+}
--- network/network_mgr.h
+++ network/network_mgr.h
@@ -22,10 +22,36 @@ public:
     * 构造
     */
     NetworkMaster();
+    /*
+    * 析构
+    */
+    virtual ~NetworkMaster();
+    /*
+    * 启动工作线程
+    */
+    virtual bool Start();
+    /*
+    * 驱动主线程内事件处理
+    */
+    virtual void Update();
+    /*
+    * 结束并等待工作线程结束,在祝县城内调用
+    */
+    virtual void StopWait();
+    /*
+    * 通知工作线程关闭连接
+    */
+    void Close(NetworkType type, uint64_t conn_id);
+    /*
+    * 通知工作线程发送数据
+    */
+    void Send(NetworkType type, uint64_t conn_id, const char* data, uint32_t size);
+
+
 private:
     Event2Main event2main_;     // 主线程网络事件队列
     std::atomic_bool stop_;     // 县城退出
-    std::unique_ptr<std::thread> worker_;   // 工作线程
+    std::unique_ptr<std::thread> worker_ = nullptr;   // 工作线程
     using NetworkArray = std::array<std::unique_ptr<INetwork>, NT_MAX>;
     NetworkArray networks_;     // 网络实现 
 };
