b09f9d14e3e029dc0bb37c9211e9f3a1e0ebaf5c
liyakai <liyakai@mail.ustc.edu.cn>
2021-05-21 10:10:23 +0800

    打印虚表及其测试.

---
+++ test/test_virtual_print.cpp
@@ -0,0 +1,49 @@
+#include "tools/virtual_print.h"
+#include "tools/unit_test_frame/unittest.h"
+
+
+FIXTURE_BEGIN(VirtualPrint)
+
+// 打印 int 类型的虚函数.[结果为没有虚函数]
+CASE(PrintInt)
+{
+    int obj = 0;
+    Virtual<int> v_obj(obj);
+    v_obj.SetDebugPrint(false);
+    v_obj.PrintVirtFunc(); 
+}
+// 定义 基类
+class Base
+{
+public:
+    // virtual ~Base(){};
+    virtual void func1(){ printf("Base::func1"); };
+    virtual void func2(){ printf("Base::func2"); };
+};
+// 定义 派生类
+class Derive : public Base
+{
+public:
+    virtual void func1(){ printf("Derive::func1"); };
+    virtual void func3(){ printf("Derive::func3"); };
+    virtual void func4(){ printf("Derive::func4"); };
+};
+// 打印 Base 类型的虚函数.
+CASE(PrintBase)
+{
+    Base obj;
+    Virtual<Base> v_obj(obj);
+    v_obj.SetDebugPrint(true);
+    v_obj.PrintVirtFunc(); 
+}
+// 打印 Derive 类型的虚函数.
+CASE(PrintDerive)
+{
+    Derive obj;
+    Virtual<Derive> v_obj(obj);
+    v_obj.SetDebugPrint(true);
+    v_obj.PrintVirtFunc(); 
+}
+
+
+FIXTURE_END(VirtualPrint)
