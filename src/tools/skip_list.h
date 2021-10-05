05bf974d1f0d35fefc933b6f0b34ab08f39981f9
liyakai <liyakai@mail.ustc.edu.cn>
2021-10-05 21:06:46 +0800

    构建跳表

--- src/tools/skip_list.h
+++ src/tools/skip_list.h
@@ -21,7 +21,7 @@ struct SkipListNode
     {
         // 下一个节点
         SkipListNode<K,V>* next = nullptr;
-        // 到下一个节点在0层相隔的节点数
+        // 到本层下一个节点相隔的节点数
         uint64_t span = 0;
     } *levels;
     SkipListNode(int32_t lev)
@@ -99,11 +99,11 @@ public:
     * @brief 插入一个新的节点,如果存在则更新
     * @param key 
     * @param value
-    * @return SkipListNode<K, V>* 节点指针
+    * @return SkipListNode<K, V>* 插入的节点指针
     */
     SkipListNode<K, V>* InsertOrUpdate(K key, V val)
     {
-        // 每层带插入节点的前置节点
+        // 每层待插入节点的前置节点
         SkipListNode<K, V>* update[SKIPLIST_MAX_LEVEL] = { nullptr };
         SkipListNode<K, V>* tmpNode = skip_list_.header;
         // 节点的排名 
@@ -120,7 +120,7 @@ public:
             while (tmpNode->levels[i].next 
                 && (tmpNode->levels[i].next -> key > key
                     || (tmpNode->levels[i].next->key == key
-                        && tmpNode->levels[i].next->val > val)))
+                        && tmpNode->levels[i].next->value > val)))
             {
                 rank[i] += tmpNode->levels[i].span;
                 tmpNode = tmpNode->levels[i].next;
@@ -132,11 +132,11 @@ public:
         if(max_len_ > 0 && skip_list_.length >= max_len_)
         {
             // 新增的数据排名最后,不进榜
-            if(update[0].levels[0].next == nullptr
-                || update[0].levels[0].next->key > key)
-                {
-                    return nullptr; 
-                }
+            if(update[0]->levels[0].next == nullptr
+                || update[0]->levels[0].next->key > key)
+            {
+                return nullptr; 
+            }
         }
         
         // 随机层数
@@ -148,11 +148,12 @@ public:
             {
                 rank[i] = 0;
                 update[i] = skip_list_.header;
-                update[i]=>levels[i].span = skip_list_.length;
+                update[i]->levels[i].span = skip_list_.length;
             }
             // 更新层数
-            skip_list.level = level;
+            skip_list_.level = level;
         }
+
         // 创建节点
         tmpNode = CreateNode(level, key, val);
         for(int32_t i = 0; i < level; i++)
@@ -162,13 +163,16 @@ public:
             update[i]->levels[i].next = tmpNode;
             // 计算新节点的span
             tmpNode->levels[i].span = update[i]->levels[i].span - (rank[0] - rank[i]);
+            // 计算新节点前置节点的span
             update[i]->levels[i].span = rank[0] - rank[i] + 1;
         }
+        
         // 新的层更新前置节点的span
         for(int32_t i = level; i < skip_list_.level; i++)
         {
             update[i]->levels[i].span++;
         }
+
         // 设置回溯节点
         tmpNode->pre = (update[0] == skip_list_.header) ? nullptr : update[0];
         if(tmpNode->levels[0].next)
@@ -179,7 +183,7 @@ public:
             skip_list_.tail = tmpNode;
         }
 
-        skip_list_.lenght++;
+        skip_list_.length++;
         // 更新map
         rank_map_.emplace(val, tmpNode);
         // 长度超出删除末尾元素
@@ -192,11 +196,39 @@ public:
     /*
     * @brief  通过元素删除节点
     * @param val 
-    * @return int32_t 0 未找到,1删除成功.
+    * @return true 删除成功; false 删除失败
     */
-    int32_t DeleteNode(V val)
+    bool DeleteNode(V val)
     {
-        return 0;
+        SkipListNode<K, V>* update[SKIPLIST_MAX_LEVEL] = {0};
+        SkipListNode<K, V>* tmpNode = skip_list_.header;
+        RankIt iter = rank_map_.find(val);
+        // 未找到节点
+        if (iter == rank_map_.end())
+        {
+            return false;
+        }
+        K& key = iter->second->key;
+        for (int32_t i = skip_list_.level - 1; i >= 0; i--)
+        {
+            while (tmpNode->levels[i]
+                    && (tmpNode->levels[i].next->key > key
+                        || (tmpNode->levels[i].next->key == key
+                            && tmpNode->levels[i].next->value > val)))
+            {
+                tmpNode = tmpNode->levels[i].next;
+            }
+            update[i] = tmpNode;
+        }
+        tmpNode = tmpNode->levels[0].next;
+        // 找到节点后删除
+        if(tmpNode && key == tmpNode->key && tmpNode->value == val)
+        {
+            DeleteNode(tmpNode, update);
+            rank_map_.erase(iter);
+            return true;
+        }
+        return false;
     }
     /*
     * @brief 通过排名删除节点
@@ -205,17 +237,41 @@ public:
     */
     uint64_t DeleteNodeByRank(uint64_t rank)
     {
-return 0;
+        return DeleteNodeByRange(rank, rank);
     }
     /*
     * @brief 通过排名区间删除节点
     * @param start 开始节点
     * @param end 结束节点
-    * @return uint64_t
+    * @return uint64_t 实际删除的节点数
     */
     uint64_t DeleteNodeByRange(uint64_t start, uint64_t end)
     {
-return 0;
+        SkipListNode<K, V>* update[SKIPLIST_MAX_LEVEL] = {0};
+        SkipListNode<K, V>* tmpNode = skip_list_.header;
+        uint64_t traversed = 0, removed = 0;
+
+        for(int32_t i = skip_list_.level - 1; i>= 0; i--)
+        {
+            while(tmpNode->levels[i].next && (traversed + tmpNode->levels[i].span) < start)
+            {
+                traversed += tmpNode->levels[i].span;
+                tmpNode = tmpNode->levels[i].next;
+            }
+            update[i] = tmpNode;
+        }
+        traversed++;
+        tmpNode = tmpNode->levels[0].next;
+        while(tmpNode && traversed <= end)
+        {
+            auto* next = tmpNode->levels[0].next;
+            DeleteNode(tmpNode, update);
+            rank_map_.erase(tmpNode->value);
+            tmpNode = next;
+            traversed++;
+            removed++;
+        }
+        return removed;
     }
     /*
     * @brief 更新节点
@@ -225,7 +281,47 @@ return 0;
     */
     SkipListNode<K, V>* UpdateNode(K key, V val)
     {
-return nullptr;
+        SkipListNode<K, V>* update[SKIPLIST_MAX_LEVEL] = {0};
+        SkipListNode<K, V>* tmpNode = skip_list_.header;
+        RankIt iter = rank_map_.find(val);
+        // 未找到节点
+        if (iter == rank_map_.end())
+        {
+            return nullptr;
+        }
+        // 当前key
+        K& cur_key = iter->second->key;
+        for (int32_t i = skip_list_.level - 1; i >= 0; i--)
+        {
+            while (tmpNode->levels[i].next
+                    && (tmpNode->levels[i].next->key > cur_key
+                        || (tmpNode->levels[i].next->key == cur_key
+                            && tmpNode->levels[i].next->value > val)))
+            {
+                tmpNode = tmpNode->levels[i].next;
+            }
+            update[i] = tmpNode;
+        }
+        tmpNode = tmpNode->levels[0].next;
+        if(nullptr == tmpNode 
+            || val != tmpNode->value
+                || cur_key != tmpNode->key)
+        {
+            // 未找到节点
+            return nullptr;
+        }
+        // 如果更新key后位置没有变化
+        if((tmpNode->pre == nullptr || tmpNode->pre->key > key)
+            && (tmpNode->levels[0].next == nullptr || tmpNode->levels[0].next->key < key))
+        {
+            tmpNode->key = key;
+            return tmpNode;
+        }
+
+        // 删除旧节点重新添加
+        DeleteNode(tmpNode, update);
+        rank_map_.erase(tmpNode->value);
+        return InsertOrUpdate(key, val);
     }
     /*
     * @brief 获取排名
@@ -296,17 +392,18 @@ private:
     /*
     * @brief 删除节点
     * @param node 被删除的节点
-    * @param update
+    * @param update 被删除节点的前置节点数组
     */
-    void DeleteNode(SkipListNode<K, V>* node, SkipListNode<K, V> ** update)
+    void DeleteNode(SkipListNode<K, V>* node, SkipListNode<K, V>** update)
     {
         if(nullptr == node || nullptr == update)
         {
             return;
         }
+        // 更新span值
         for(int32_t i = 0; i < skip_list_.level; i++)
         {
-            if(node -= update[i]->levels[i].next)
+            if(node == update[i]->levels[i].next)
             {
                 update[i]->levels[i].span += node->levels[i].span - 1;
                 update[i]->levels[i].next = node->levels[i].next;
@@ -315,7 +412,7 @@ private:
                 update[i]->levels[i].span--;
             }
         }
-
+        // 更新pre和tail
         if(node->levels[0].next)
         {
             node->levels[0].next->pre = node->pre;
@@ -329,11 +426,18 @@ private:
         {
             skip_list_.level--;
         }
-        skip_list_.length--;
 
+        skip_list_.length--;
+        delete node;
     }
     /*
     * @brief 随机层级
+    * 理论来讲，一级索引中元素个数应该占原始数据的 50%，二级索引中元素个数占 25%，三级索引12.5% ，一直到最顶层。
+    * 因为这里每一层的晋升概率是 50%。对于每一个新插入的节点，都需要调用 randomLevel 生成一个合理的层数。
+    * 该 randomLevel 方法会随机生成 1~MAX_LEVEL 之间的数，且 ：
+    *        50%的概率返回 1
+    *        25%的概率返回 2
+    *      12.5%的概率返回 3 ...
     * @return 随机值
     */
     int32_t RandomLevel()
