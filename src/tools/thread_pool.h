1bf948ad634da6485aa7d7e458f9f9a841a02b4d
liyakai <liyakai@mail.ustc.edu.cn>
2021-12-05 15:58:33 +0800

    实现线程池及测试用例

---
+++ src/tools/thread_pool.h
@@ -0,0 +1,120 @@
+#pragma once
+#include <vector>
+#include <thread>
+#include <functional>
+#include <future>
+#include <queue>
+#include <mutex>
+#include <condition_variable>
+#include <stdint.h>
+
+/*
+*  定义线程池
+*/
+class ThreadPool
+{
+public: 
+    typedef std::function<void()> task_type;
+    /*
+    * 构造
+    */
+    explicit ThreadPool(int32_t n = 0);
+    /*
+    * 析构
+    */
+    ~ThreadPool()
+    {
+        Stop();
+        cond_.notify_all();
+        for (auto& thread : threads_)
+        {
+            if (thread.joinable())
+            {
+                thread.join();
+            } 
+        }
+    }
+    /*
+    * 停止线程池
+    */
+    void Stop()
+    {
+        stop_.store(true, std::memory_order_release);
+    }
+    /*
+    * 增加任务
+    */
+    template<typename Function, typename... Args>
+    std::future<typename std::result_of<Function(Args...)>::type> Add(Function&&, Args&&...);
+private:
+    ThreadPool(ThreadPool&& tp) = delete;
+    ThreadPool& operator = (ThreadPool&& tp) = delete;
+
+    ThreadPool(const ThreadPool& tp) = delete;
+    ThreadPool& operator = (const ThreadPool&& tp) = delete;
+
+private:
+    /// 原子量: 是否停止线程池
+    std::atomic<bool> stop_;
+    /// 互斥锁
+    std::mutex mtx_;
+    /// 条件变量,阻塞工作线程,以及通知有任务产生
+    std::condition_variable cond_;
+    /// 任务队列
+    std::queue<task_type> tasks_;
+    /// 线程池
+    std::vector<std::thread> threads_;
+};
+
+inline ThreadPool::ThreadPool(int32_t n)
+    : stop_(false)
+    {
+        int32_t nthreads = n;
+        if(nthreads <= 0)
+        {
+            nthreads = std::thread::hardware_concurrency();
+            nthreads = (nthreads == 0 ? 2 : nthreads);
+        }
+        for (int32_t i = 0; i < nthreads; i++)
+        {
+            threads_.emplace_back(std::thread([this]{
+                while (!stop_.load(std::memory_order_acquire))
+                {
+                    task_type task;
+                    {
+                        std::unique_lock<std::mutex> ulk(this->mtx_);
+                        this->cond_.wait(ulk, [this]{ return stop_.load(std::memory_order_acquire) || (!this->tasks_.empty()); });
+                        if(stop_.load(std::memory_order_acquire))
+                        {
+                            return;
+                        }
+                        task = std::move(this->tasks_.front());
+                        this->tasks_.pop();
+                    }
+                    task();
+                }
+                
+            }));
+        }
+    }
+
+template<typename Function, typename... Args>
+std::future<typename std::result_of<Function(Args...)>::type>
+ThreadPool::Add(Function&& func, Args&&... args)
+{
+    using return_type = typename std::result_of<Function(Args...)>::type;
+    using task = std::packaged_task<return_type()>;
+
+    auto tk = std::make_shared<task>(std::bind(std::forward<Function>(func), std::forward<Args>(args)...));
+    auto ret = tk->get_future();
+    {
+        std::lock_guard<std::mutex> lg(mtx_);
+        if(stop_.load(std::memory_order_acquire))
+        {
+            throw std::runtime_error("thread pool has stopped.");
+        }
+        tasks_.emplace([tk]{(*tk)();});
+    }
+    cond_.notify_one();
+    return ret;
+}
---
+++ test/test_thread_pool.cpp
@@ -0,0 +1,55 @@
+#include "src/tools/thread_pool.h"
+#include "unit_test_frame/unittest.h"
+#include <map>
+
+FIXTURE_BEGIN(TestThreadPool)
+
+// ²âÊÔÊý×ÖÓÐ·µ»Ø
+CASE(TestThreadPoolCase1_num){
+    std::mutex mtx;
+    try{
+        ThreadPool thread_pool;
+        std::vector<std::future<int32_t>> v_future;
+        for (int32_t i = 0; i < 10; i++)
+        {
+            auto res = thread_pool.Add([](int32_t answer)->int32_t{ return answer;}, i);
+            v_future.emplace_back(std::move(res));
+        }
+        for (size_t i = 0; i < v_future.size(); i++)
+        {
+            std::lock_guard<std::mutex> lg(mtx);
+            fprintf(stderr, "threadpool result: %d\n", v_future[i].get());
+        }
+    } catch(std::exception& e)
+    {
+        fprintf(stderr, "[TestThreadPoolCase] %s \n", e.what());
+    }
+}
+
+// ²âÊÔ×Ö·û´®ÎÞ·µ»Ø
+CASE(TestThreadPoolCase1_num_str){
+    std::mutex mtx;
+    try{
+        ThreadPool thread_pool;
+        std::vector<std::future<void>> v_future;
+        for (int32_t i = 0; i <= 5; i++)
+        {
+            auto res = thread_pool.Add([&mtx](const std::string& str1, const std::string& str2){
+                std::lock_guard<std::mutex> lg(mtx);
+                fprintf(stderr, "%s%s\n", str1.c_str(), str2.c_str());
+            }, "hello ", "world");
+            v_future.emplace_back(std::move(res));
+        }
+        for (size_t i = 0; i < v_future.size(); i++)
+        {
+            v_future[i].get();
+        }
+        
+        
+    } catch(std::exception& e)
+    {
+        fprintf(stderr, "[TestThreadPoolCase] %s \n", e.what());
+    }
+}
+
+FIXTURE_END(TestThreadPool)
