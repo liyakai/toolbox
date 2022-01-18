eb3cdf386013a73e3d0036f3fb7a5f9b6231db88
liyakai <liyakai@mail.ustc.edu.cn>
2022-01-18 18:11:11 +0800

    增加测试用例

--- src/tools/string_util.h
+++ src/tools/string_util.h
@@ -1,5 +1,8 @@
 # pragma once
+#include <vector>
+#include <string>
 
+namespace ToolBox{
 
 /*
 * @brief 字符串分割函数
@@ -7,19 +10,19 @@
 * @param tokens 已分割字符串
 * @param dekunuters 分割字符
 */
-void Split(const std::string str, vector<std::string>& tokens, const string& delimiters = " ")
+void Split(const std::string str, std::vector<std::string>& tokens, const std::string& delimiters = " ")
 {
-	// 在字符串str中查找第一个与delimiters中的字符都不匹配的字符，返回它的位置。搜索从index=0开始。如果没找到就返回string::nops
-	string::size_type last_pos = str.find_first_not_of(delimiters, 0);
+	// 在字符串str中查找第一个与delimiters中的任意字符都不匹配的字符，返回它的位置。搜索从index=0开始。如果没找到就返回string::nops
+	std::string::size_type last_pos = str.find_first_not_of(delimiters, 0);
 	// 在字符串str中从last_pos位置开始查找第一个与delimiters匹配的字符,返回它的位置.如果没炸到就返回 string::nops
-	string::size_type pos = str.find_first_of(delimiters, last_pos);
-	while (string::npos != pos || strubg::npos != last_pos)
+	std::string::size_type pos = str.find_first_of(delimiters, last_pos);
+	while (std::string::npos != pos || std::string::npos != last_pos)
 	{
-		tokens.emplace_back(s.substr(las_pos, pos - last_pos));
+		tokens.emplace_back(str.substr(last_pos, pos - last_pos));
 		last_pos = str.find_first_not_of(delimiters, pos);
 		pos = str.find_first_of(delimiters, last_pos);
 	}
 }
 
 
-
+}
--- test/test_string_util.cpp
+++ test/test_string_util.cpp
@@ -1,16 +1,15 @@
-#include "src/tools/smart_pointer.h"
+#include "src/tools/string_util.h"
 #include "unit_test_frame/unittest.h"
 
-
-FIXTURE_BEGIN(TestSmartPointer)
+FIXTURE_BEGIN(TestStringUtil)
 
 // 使用默认空格分隔符
 CASE(test_space_delimiters)
 {
     std::vector<std::string> result;
-    std::string string_with_space = " li ya kai too box .";
-    Split(string_with_space, result);
-    fprintf(stderr, "[字符串分割测试] 默认空格分割测试结果:\n");
+    std::string string_with_space = " li ya kai tool box .";
+    ToolBox::Split(string_with_space, result);
+    fprintf(stderr, "[字符串分割测试] 默认空格分割测试原始字符串[%s],分割结果:\n",string_with_space.c_str());
     for (auto &str : result)
     {
         fprintf(stderr, "%s\n", str.c_str());
@@ -18,7 +17,19 @@ CASE(test_space_delimiters)
     
 }
 
+// 使用多分隔符"|"
+CASE(test_char_delimiters)
+{
+    std::vector<std::string> result;
+    std::string string_with_space = " li|ya| kai |tool||| box|||||| .";
+    ToolBox::Split(string_with_space, result, "||");
+    fprintf(stderr, "[字符串分割测试] 竖线\"|\"分隔符测试原始字符串[%s],分割结果:\n",string_with_space.c_str());
+    for (auto &str : result)
+    {
+        fprintf(stderr, "%s\n", str.c_str());
+    }
+}
 
 
 
-FIXTURE_END(TestSmartPointer)
+FIXTURE_END(TestStringUtil)
