d398c8f4f93dc536a499ac85a127cb334f1389dd
liyakai <liyakai@mail.ustc.edu.cn>
2022-07-21 21:15:29 +0800

    ccache缓存编译提速.

---
+++ cmake/ccache.cmake
@@ -0,0 +1,6 @@
+
+find_program(CCACHE_FOUND ccache)
+if(CCACHE_FOUND)
+    set_property(GLOBAL PROPERTY RULE_LAUNCH_COMPILE ccache)
+    set_property(GLOBAL PROPERTY RULE_LAUNCH_LINK ccache) # Less useful to do it for linking, see edit2
+endif(CCACHE_FOUND)
