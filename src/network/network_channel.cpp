2bd91d4bac3724c613380d8edea6e77a86697c12
liyakai <liyakai@bytedance.com>
2023-10-03 02:12:37 +0800

    fix thread index bug

--- src/network/network_channel.cpp
+++ src/network/network_channel.cpp
@@ -256,6 +256,10 @@ namespace ToolBox
         static std::default_random_engine dre(time(0));  // 稍微随机些的种子
         std::uniform_int_distribution<unsigned > uid(0, networks_.size() - 1);
         uint32_t random_thread_index = uid(dre);
+        if (0 == networks_.size())
+        {
+            random_thread_index = 0;
+        }
         NetworkLogInfo("[Network] Push connect event to network_thread_index:%u", random_thread_index);
         NotifyWorker(event, type, random_thread_index);      //随机去某个线程中去连接.
     }
