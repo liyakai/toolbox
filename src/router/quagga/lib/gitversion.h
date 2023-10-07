98295ce55f93c2382e9db66c01a113976499f2fc
liyakai <liyakai@mail.ustc.edu.cn>
2023-10-07 22:16:57 +0800

    补充之前过滤掉的文件.

---
+++ src/router/quagga/lib/gitversion.h
@@ -0,0 +1,7 @@
+#define GIT_SUFFIX "-g58552d28f"
+#define GIT_INFO "\r\n\
+This is a git build of frr-8.4-rc-167-g58552d28f\r\n\
+Associated branch(es):\r\n\
+\tlocal:master\r\n\
+\tgithub/FRRouting/frr.git/master\r\n\
+"
