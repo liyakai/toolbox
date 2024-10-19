f8ac745c0fcaffb22ecbb46cf83f92c4f9fee7ea
bryndenli <bryndenli@tencent.com>
2024-10-20 01:03:09 +0800

    update the timer interface.

--- test/test_timer.cpp
+++ test/test_timer.cpp
@@ -4,21 +4,22 @@
 
 FIXTURE_BEGIN(Timer)
 
-class TestTimer : public ToolBox::ITimer
-{
-public:
-    void OnTimer(uint32_t id, uint32_t count) override
-    {
-        fprintf(stderr, "触发ITimer 类型定时器,当前定时器id:%u,count:%u\n", id, count);
-    }
-};
+// class TestTimer : public ToolBox::ITimer
+// {
+// public:
+//     void OnTimer(uint32_t id, uint32_t count) override
+//     {
+//         fprintf(stderr, "触发ITimer 类型定时器,当前定时器id:%u,count:%u\n", id, count);
+//     }
+// };
 
 // 测试ITimer触发10次
 CASE(TimerCase1)
 {
     return;
-    auto timer = std::make_shared<TestTimer>();
-    ToolBox::TimerMgr->AddTimer(timer, 10086, 1000, 10, __FILE__, __LINE__);
+    ToolBox::TimerMgr->AddTimer([](int times) {
+      fprintf(stderr, "触发ITimer 类型定时器,当前定时器触发第 %d 次\n", times);
+    }, 1000, 10, __FILE__, __LINE__);
     while (true)
     {
         std::this_thread::sleep_for(std::chrono::milliseconds(100));
@@ -30,8 +31,9 @@ CASE(TimerCase1)
 CASE(TimerCase2)
 {
     return;
-    auto timer = std::make_shared<TestTimer>();
-    ToolBox::TimerMgr->AddTimer(timer, 10087, 1000, -1, __FILE__, __LINE__);
+    ToolBox::TimerMgr->AddTimer([](int times) {
+      fprintf(stderr, "触发ITimer 类型定时器,当前定时器触发第 %d 次\n", times);
+    }, 1000, -1, __FILE__, __LINE__);
     while (true)
     {
         std::this_thread::sleep_for(std::chrono::milliseconds(100));
@@ -39,125 +41,32 @@ CASE(TimerCase2)
     }
 }
 
-// 测试将 ITimer 对象添加到定时器后立马 ITimer 对象的情况.
+// 测试将 ITimer 对象添加到定时器后立马删除参数的情况.
 CASE(TimerCase3)
 {
     return;
-    auto timer = std::make_shared<TestTimer>();
-    ToolBox::TimerMgr->AddTimer(timer, 10087, 1000, 10, __FILE__, __LINE__);
+    struct TimerArgs
+    {
+        int param1;
+        std::string param2;
+    };
+    auto timer = std::make_shared<TimerArgs>();
+    timer->param1 = 100;
+    timer->param2 = "参数2";
+    // 使用 weak_ptr 来接收参数
+    std::weak_ptr<TimerArgs> weak_timer = timer;
+    ToolBox::TimerMgr->AddTimer([&weak_timer](int times) {
+      auto arg = weak_timer.lock();
+      if (arg)
+      {
+        fprintf(stderr, "触发ITimer 类型定时器,当前定时器触发第 %d 次, param1:%d, param2:%s\n", times, arg->param1, arg->param2.c_str());
+      }else
+      {
+        fprintf(stderr, "触发ITimer 类型定时器,当前定时器触发第 %d 次, 参数已销毁.\n", times);
+      }
+    }, 1000, 10, __FILE__, __LINE__);
     timer.reset();
-    while (false) // 打开测试将这里改为 true
-    {
-        std::this_thread::sleep_for(std::chrono::milliseconds(100));
-        fprintf(stderr, "[定时器测试3] TimerMgr->Update() \n");
-        ToolBox::TimerMgr->Update();
-    }
-}
-
-class LambdaArgs : public ToolBox::IArgs
-{
-public:
-    int32_t arg1 = 0;
-    std::string arg2;
-};
-static auto lambda = [](std::weak_ptr<ToolBox::IArgs> iargs, std::weak_ptr<void> arg)->bool
-{
-    auto args = iargs.lock();
-    if (nullptr == args)
-    {
-        fprintf(stderr, "[lambda]参数 iargs 为空指针.\n");
-        return false;
-    }
-    auto* largs = dynamic_cast<LambdaArgs*>(args.get());
-    if (nullptr == largs)
-    {
-        fprintf(stderr, "[lambda]参数 args 向子类转换失败.\n");
-        return false;
-    }
-    fprintf(stderr, "触发 回调 类型定时器 lambda.参数为 arg1:%d,arg2:%s.\n", largs->arg1, largs->arg2.c_str());
-    return true;
-};
-// 测试回调lambda
-CASE(TimerCase4)
-{
-    return;
-    auto largs = std::make_shared<LambdaArgs>();
-    largs->arg1 = 100;
-    largs->arg2 = "参数2";
-    ToolBox::TimerMgr->AddTimer(lambda, largs, std::weak_ptr<void>(), 1000, -1, __FILE__, __LINE__);
     while (true)
-    {
-        std::this_thread::sleep_for(std::chrono::milliseconds(100));
-        ToolBox::TimerMgr->Update();
-    }
-}
-
-// 测试回调lambda 参数AddTimer之后立马重置
-CASE(TimerCase5)
-{
-    return;
-    auto largs = std::make_shared<LambdaArgs>();
-    largs->arg1 = 100;
-    largs->arg2 = "参数2";
-    ToolBox::TimerMgr->AddTimer(lambda, largs, std::weak_ptr<void>(), 1000, -1, __FILE__, __LINE__);
-    largs.reset();
-    while (true)
-    {
-        std::this_thread::sleep_for(std::chrono::milliseconds(100));
-        ToolBox::TimerMgr->Update();
-    }
-}
-
-class TestTimerDelegate
-{
-public:
-    bool OnTimer(std::weak_ptr<ToolBox::IArgs> iargs, std::weak_ptr<void> arg)
-    {
-        auto args = iargs.lock();
-        if (nullptr == args)
-        {
-            fprintf(stderr, "[TestTimerDelegate]参数 iargs 为空指针.\n");
-            return false;
-        }
-        auto* largs = dynamic_cast<LambdaArgs*>(args.get());
-        if (nullptr == largs)
-        {
-            fprintf(stderr, "[TestTimerDelegate]参数 args 向子类转换失败.\n");
-            return false;
-        }
-        fprintf(stderr, "触发 Delegate 类型定时器 .参数为 arg1:%d,arg2:%s.\n", largs->arg1, largs->arg2.c_str());
-        return true;
-    }
-};
-
-// 测试委托
-CASE(TimerCase6)
-{
-    return;
-    auto timer = std::make_shared<TestTimerDelegate>();
-    auto largs = std::make_shared<LambdaArgs>();
-    largs->arg1 = 100;
-    largs->arg2 = "参数2";
-    ToolBox::TimerMgr->AddTimer(DelegateCombination(TestTimerDelegate, OnTimer, timer), largs, std::weak_ptr<void>(), 1000, -1, __FILE__, __LINE__);
-    while (true) // 打开测试将这里改为 true
-    {
-        std::this_thread::sleep_for(std::chrono::milliseconds(100));
-        ToolBox::TimerMgr->Update();
-    }
-}
-
-// 测试委托 测试对象和参数在AddTimer后立马重置
-CASE(TimerCase7)
-{
-    return;
-    auto timer = std::make_shared<TestTimerDelegate>();
-    auto largs = std::make_shared<LambdaArgs>();
-    largs->arg1 = 100;
-    largs->arg2 = "参数2";
-    ToolBox::TimerMgr->AddTimer(DelegateCombination(TestTimerDelegate, OnTimer, timer), largs, std::weak_ptr<void>(), 1000, -1, __FILE__, __LINE__);
-    timer.reset();
-    largs.reset();
-    while (true) // 打开测试将这里改为 true
     {
         std::this_thread::sleep_for(std::chrono::milliseconds(100));
         ToolBox::TimerMgr->Update();
