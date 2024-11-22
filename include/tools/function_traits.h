f72dd6844ac0d2864ede735a0c2b3877408f0457
liyakai <liyakai@mail.ustc.edu.cn>
2024-11-23 01:26:46 +0800

    optimize function traits

--- include/tools/function_traits.h
+++ include/tools/function_traits.h
@@ -6,24 +6,188 @@
 namespace ToolBox {
 
 
-// Add function_traits definition
+// Add FunctionTraits definition
 template<typename F>
-struct function_traits;
+struct FunctionTraits;
 
-// For regular functions
-template<typename R, typename... Args>
-struct function_traits<R(Args...)> {
-    using return_type = R;
-    using args_tuple = std::tuple<Args...>;
-    static constexpr size_t arity = sizeof...(Args);
+template <typename Return, typename... Arguments>
+struct FunctionTraits<Return (*)(Arguments...)> {
+  using parameters_type = std::tuple<std::remove_cvref_t<Arguments>...>;
+  using return_type = Return;
 };
 
-// For member functions
-template<typename R, typename C, typename... Args>
-struct function_traits<R(C::*)(Args...)> {
-    using return_type = R;
-    using args_tuple = std::tuple<Args...>;
-    static constexpr size_t arity = sizeof...(Args);
+template <typename Return, typename... Arguments>
+struct FunctionTraits<Return (*)(Arguments...) noexcept> {
+  using parameters_type = std::tuple<std::remove_cvref_t<Arguments>...>;
+  using return_type = Return;
 };
+
+template <typename Return, typename... Arguments>
+struct FunctionTraits<Return(Arguments...)> {
+  using parameters_type = std::tuple<std::remove_cvref_t<Arguments>...>;
+  using return_type = Return;
+};
+
+template <typename Return, typename... Arguments>
+struct FunctionTraits<Return(Arguments...) noexcept> {
+  using parameters_type = std::tuple<std::remove_cvref_t<Arguments>...>;
+  using return_type = Return;
+};
+
+template <typename This, typename Return, typename... Arguments>
+struct FunctionTraits<Return (This::*)(Arguments...)> {
+  using parameters_type = std::tuple<std::remove_cvref_t<Arguments>...>;
+  using return_type = Return;
+  using class_type = This;
+};
+
+template <typename This, typename Return, typename... Arguments>
+struct FunctionTraits<Return (This::*)(Arguments...) noexcept> {
+  using parameters_type = std::tuple<std::remove_cvref_t<Arguments>...>;
+  using return_type = Return;
+  using class_type = This;
+};
+
+template <typename This, typename Return, typename... Arguments>
+struct FunctionTraits<Return (This::*)(Arguments...) const> {
+  using parameters_type = std::tuple<std::remove_cvref_t<Arguments>...>;
+  using return_type = Return;
+  using class_type = This;
+};
+
+template <typename This, typename Return, typename... Arguments>
+struct FunctionTraits<Return (This::*)(Arguments...) const noexcept> {
+  using parameters_type = std::tuple<std::remove_cvref_t<Arguments>...>;
+  using return_type = Return;
+  using class_type = This;
+};
+
+template <typename Return>
+struct FunctionTraits<Return (*)()> {
+  using parameters_type = void;
+  using return_type = Return;
+};
+
+template <typename Return>
+struct FunctionTraits<Return (*)() noexcept> {
+  using parameters_type = void;
+  using return_type = Return;
+};
+
+template <typename Return>
+struct FunctionTraits<Return (&)()> {
+  using parameters_type = void;
+  using return_type = Return;
+};
+
+template <typename Return>
+struct FunctionTraits<Return (&)() noexcept> {
+  using parameters_type = void;
+  using return_type = Return;
+};
+
+template <typename Return>
+struct FunctionTraits<Return()> {
+  using parameters_type = void;
+  using return_type = Return;
+};
+
+template <typename Return>
+struct FunctionTraits<Return() noexcept> {
+  using parameters_type = void;
+  using return_type = Return;
+};
+
+template <typename This, typename Return>
+struct FunctionTraits<Return (This::*)()> {
+  using parameters_type = void;
+  using return_type = Return;
+  using class_type = This;
+};
+
+template <typename This, typename Return>
+struct FunctionTraits<Return (This::*)() noexcept> {
+  using parameters_type = void;
+  using return_type = Return;
+  using class_type = This;
+};
+
+template <typename This, typename Return>
+struct FunctionTraits<Return (This::*)() const> {
+  using parameters_type = void;
+  using return_type = Return;
+  using class_type = This;
+};
+
+template <typename This, typename Return>
+struct FunctionTraits<Return (This::*)() const noexcept> {
+  using parameters_type = void;
+  using return_type = Return;
+  using class_type = This;
+};
+
+// Support function object and lambda expression
+template <class Function>
+struct FunctionTraits : FunctionTraits<decltype(&Function::operator())> {};
+
+template <typename Function>
+using function_parameters_t =
+    typename FunctionTraits<std::remove_cvref_t<Function>>::parameters_type;
+
+
+template <typename Function>
+using last_parameters_type_t =
+    std::tuple_element_t<std::tuple_size_v<function_parameters_t<Function>> - 1,
+                         function_parameters_t<Function>>;
+
+template <typename Function>
+using function_return_type_t =
+    typename FunctionTraits<std::remove_cvref_t<Function>>::return_type;
+
+template <typename Function>
+using class_type_t =
+    typename FunctionTraits<std::remove_cvref_t<Function>>::class_type;
+
+template <typename F, typename... Args>
+struct is_invocable
+    : std::is_constructible<
+          std::function<void(std::remove_reference_t<Args>...)>,
+          std::reference_wrapper<typename std::remove_reference<F>::type>> {};
+
+template <typename F, typename... Args>
+inline constexpr bool is_invocable_v = is_invocable<F, Args...>::value;
+
+template <typename T>
+struct remove_first {
+  using type = T;
+};
+
+template <class First, class... Second>
+struct remove_first<std::tuple<First, Second...>> {
+  using type = std::tuple<Second...>;
+};
+
+template <typename T>
+using remove_first_t = typename remove_first<T>::type;
+
+template <bool has_conn, typename T>
+inline auto get_args() {
+  if constexpr (has_conn) {
+    using args_type = remove_first_t<T>;
+    return args_type{};
+  }
+  else {
+    return T{};
+  }
+}
+
+template <typename Test, template <typename...> class Ref>
+struct is_specialization : std::false_type {};
+
+template <template <typename...> class Ref, typename... Args>
+struct is_specialization<Ref<Args...>, Ref> : std::true_type {};
+
+template <typename Test, template <typename...> class Ref>
+inline constexpr bool is_specialization_v = is_specialization<Test, Ref>::value;
 
 }   // namespace ToolBox
--- test/test_function_traits.h
+++ test/test_function_traits.h
@@ -18,13 +18,13 @@ public:
 
 CASE(FunctionTraitsTest_RegularFunction) {
     // Test regular function with multiple parameters
-    using traits1 = ToolBox::function_traits<decltype(test_func)>;
+    using traits1 = ToolBox::FunctionTraits<decltype(test_func)>;
     static_assert(std::is_same_v<traits1::return_type, int>);
     static_assert(traits1::arity == 2);
     static_assert(std::is_same_v<traits1::args_tuple, std::tuple<int, double>>);
 
     // Test regular function with no parameters
-    using traits2 = ToolBox::function_traits<decltype(test_func2)>;
+    using traits2 = ToolBox::FunctionTraits<decltype(test_func2)>;
     static_assert(std::is_same_v<traits2::return_type, std::string>);
     static_assert(traits2::arity == 0);
     static_assert(std::is_same_v<traits2::args_tuple, std::tuple<>>);
@@ -32,13 +32,13 @@ CASE(FunctionTraitsTest_RegularFunction) {
 
 CASE(FunctionTraitsTest_MemberFunction) {
     // Test non-const member function
-    using traits1 = ToolBox::function_traits<decltype(&TestClass::member_func)>;
+    using traits1 = ToolBox::FunctionTraits<decltype(&TestClass::member_func)>;
     static_assert(std::is_same_v<traits1::return_type, void>);
     static_assert(traits1::arity == 2);
     static_assert(std::is_same_v<traits1::args_tuple, std::tuple<int, float>>);
 
     // Test const member function
-    using traits2 = ToolBox::function_traits<decltype(&TestClass::const_member_func)>;
+    using traits2 = ToolBox::FunctionTraits<decltype(&TestClass::const_member_func)>;
     static_assert(std::is_same_v<traits2::return_type, int>);
     static_assert(traits2::arity == 1);
     static_assert(std::is_same_v<traits2::args_tuple, std::tuple<std::string>>);
@@ -46,13 +46,13 @@ CASE(FunctionTraitsTest_MemberFunction) {
 
 CASE(FunctionTraitsTest_Lambda) {
     auto lambda1 = [](int x, double y) -> float { return 0.0f; };
-    using traits1 = ToolBox::function_traits<decltype(lambda1)>;
+    using traits1 = ToolBox::FunctionTraits<decltype(lambda1)>;
     static_assert(std::is_same_v<traits1::return_type, float>);
     static_assert(traits1::arity == 2);
     static_assert(std::is_same_v<traits1::args_tuple, std::tuple<int, double>>);
 
     auto lambda2 = []() -> void {};
-    using traits2 = ToolBox::function_traits<decltype(lambda2)>;
+    using traits2 = ToolBox::FunctionTraits<decltype(lambda2)>;
     static_assert(std::is_same_v<traits2::return_type, void>);
     static_assert(traits2::arity == 0);
     static_assert(std::is_same_v<traits2::args_tuple, std::tuple<>>);
